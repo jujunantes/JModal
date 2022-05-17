b96c3d4c7c5d08062b47038a8f76d255df64f6f3
Julien MARCEL <jul_marcel@hotmail.com>
2022-05-17 17:41:34 +0200

    css tweeking

--- dist/index.js
+++ dist/index.js
@@ -1,53 +1,20 @@
-"use strict";
+import React from 'react'
+import './JModal.css'
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
-var _react = _interopRequireDefault(require("react"));
-
-require("./JModal.css");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function JModal(_ref) {
-  let {
-    title,
-    text,
-    btn_text,
-    closingModale
-  } = _ref;
-
-  if (title === undefined) {
-    title = 'Lorem Ipsum';
-  }
-
-  if (text === undefined) {
-    text = 'Lorem Ipsum';
-  }
-
-  if (btn_text === undefined) {
-    btn_text = 'OK';
-  }
-
-  return /*#__PURE__*/_react.default.createElement("div", {
-    id: "confirmation",
-    className: "JModal"
-  }, /*#__PURE__*/_react.default.createElement("div", {
-    className: "JModal_window"
-  }, /*#__PURE__*/_react.default.createElement("h3", {
-    className: "JModal_h3"
-  }, title), /*#__PURE__*/_react.default.createElement("p", {
-    className: "JModal_p"
-  }, text), /*#__PURE__*/_react.default.createElement("p", {
-    className: "JModal_close-button",
-    onClick: closingModale
-  }, "\u2716"), /*#__PURE__*/_react.default.createElement("p", null, /*#__PURE__*/_react.default.createElement("button", {
-    className: "JModal_button",
-    onClick: closingModale
-  }, btn_text))));
+function JModal({title, text, btn_text,  closingModale}) {
+    if (title === undefined) {title = 'Lorem Ipsum'}
+    if (text === undefined) {text = 'Lorem Ipsum'}
+    if (btn_text === undefined) {btn_text = 'OK'}
+    return (
+        <div id="confirmation" className='JModal'>
+            <div className='JModal_window'>
+                <h3 className='JModal_h3'>{title}</h3>
+                <p className='JModal_p'>{text}</p>
+                <p className='JModal_close-button' onClick={closingModale}>&#10006;</p>
+                <p><button className="JModal_button" onClick={closingModale}>{btn_text}</button></p>
+            </div>
+        </div>
+    )
 }
 
-var _default = JModal;
-exports.default = _default;
+export default JModal
